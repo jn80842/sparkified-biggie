63809e04e32eaf2aa681d419ebf8f2839d9847dc
Julie Newcomb <jn80842@gmail.com>
2013-03-30 17:24:36 -0700

    fix simplified args

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 sparkified-biggie
 =================
+Call on chr20
+sbt/sbt "run-main biggiesparks.SimpleCaller /scratch/rxia/other_data/insert400/bwa/Venter_chr20_sorted.sam"
--- src/main/scala/SimpleCaller.scala
+++ src/main/scala/SimpleCaller.scala
@@ -10,7 +10,7 @@ object SimpleCaller {
     //dead simple first version
     val sc = new SparkContext("local", "SimpleCaller", "/data/spark-0.7.0", List("target/scala-2.9.2/sparkifiedbiggie_2.9.2-0.0.1.jar"))
 
-    val reads = SAM.read(args(1))
+    val reads = SAM.read(args(0))
     val readSeq = reads.toIndexedSeq
     val readRDD = sc.parallelize(readSeq)
 
