442d0c0c4b8973d5a576648fb068cd17aa0882b3
Julie Newcomb <jn80842@gmail.com>
2013-03-30 23:03:50 -0700

    try treating textfile elements as reads

--- src/main/scala/SimpleCaller.scala
+++ src/main/scala/SimpleCaller.scala
@@ -12,9 +12,11 @@ object SimpleCaller {
     val sc = new SparkContext("local", "SimpleCaller", "/home/eecs/jnewcomb/spark-0.7.0", 
       List("target/scala-2.9.2/sparkifiedbiggie_2.9.2-0.0.1.jar"))
 
+    //val accum = sc.accumulable(0)
+
     val reads = sc.textFile(args(0))
 
-    val count = reads.count()
+    val count = reads.filter(!_.startsWith("@")).count()
     println(count)
 
     //args(0) reference
