e5dadc386aaaedb7289359336e7c96ffda3bf572
Julie Newcomb <jn80842@gmail.com>
2013-03-30 21:56:51 -0700

    wrong location for spark on watson

--- src/main/scala/SimpleCaller.scala
+++ src/main/scala/SimpleCaller.scala
@@ -9,7 +9,7 @@ object SimpleCaller {
 
   def main(args: Array[String]) {
     //dead simple first version
-    val sc = new SparkContext("local", "SimpleCaller", "/data/spark-0.7.0", List("target/scala-2.9.2/sparkifiedbiggie_2.9.2-0.0.1.jar"))
+    val sc = new SparkContext("local", "SimpleCaller", "../spark-0.7.0", List("target/scala-2.9.2/sparkifiedbiggie_2.9.2-0.0.1.jar"))
 
     val lines = Source.fromFile(args(0)).getLines.toIndexedSeq
 
