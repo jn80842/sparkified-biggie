274613c8bf8c59be4b72cdf72fd98e241357c7b1
Julie Newcomb <jn80842@gmail.com>
2013-04-01 22:25:03 -0700

    check some assumptions

--- src/main/scala/SimpleCaller.scala
+++ src/main/scala/SimpleCaller.scala
@@ -20,14 +20,15 @@ object SimpleCaller {
     //val accum = sc.accumulable(0)
 
     val reads = sc.textFile(args(0))
-    val ref = FASTA.read(args(1))
-    val broadcastRef = sc.broadcast(ref.pieces(0))
+    println(reads.first())
+    //val ref = FASTA.read(args(1))
+    //val broadcastRef = sc.broadcast(ref.pieces(0))
 
-    val count = reads.flatMap(runSimpleCaller(broadcastRef.value, _)).count()
+    //val count = reads.flatMap(runSimpleCaller(broadcastRef.value, _)).count()
     //val count = snps.count()
     // val count = reads.filter(!_.startsWith("@")).map(SamParse.parse).distinct().count()
     // println(count)
-    println(count)
+    //println(count)
 
     //args(0) reference
     //args(1) read file
